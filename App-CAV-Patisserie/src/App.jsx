404cdcdb0c01591dac13ad087f4aab98ea568ee9
Sibli Cav <cav.sibligs@gmail.com>
2024-09-10 15:06:27 +0200

    initial commit

---
+++ App-CAV-Patisserie/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import './App.css'; // Garde les styles généraux
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <header className="app-header">
+          <h1>La pâtisserie 3WA</h1>
+          <nav>
+            <Link to="/home">Home</Link>
+            <Link to="/login">Login</Link>
+          </nav>
+        </header>
+        
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
